ed84b775f1b42afd0b2bba3aedc451e21c1e819c
balcoder <des.barrett@gmail.com>
2024-07-04 15:52:03 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!!");
