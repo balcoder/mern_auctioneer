7d94009eb10b08436280acd1e0ba742eb74c37ac
balcoder <des.barrett@gmail.com>
2024-07-13 13:40:21 +0100

    added create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter  from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 
@@ -27,6 +28,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 //catch errors
 app.use((err, req, res, next) => {
