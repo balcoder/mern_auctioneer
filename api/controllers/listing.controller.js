5776a5e110ea884c3d50b42b392abfed3e684afa
balcoder <des.barrett@gmail.com>
2024-07-16 15:07:26 +0100

    created update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 import Listing from "../models/listing.modle.js";
 import { errorHandler } from "../utils/errors.js";
+import mongoose from 'mongoose';
 
 export const createListing = async (req, res, next) => {
     try {
@@ -9,7 +10,7 @@ export const createListing = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
 
 export const deleteListing = async (req, res, next) => {
    
@@ -28,5 +29,35 @@ export const deleteListing = async (req, res, next) => {
     } catch (error) {
         next(error);
         
+    }
+};
+
+export const updateListing = async (req, res, next) => {
+    const listingId = req.params.id;
+
+    // Validate the listingId
+    if (!mongoose.Types.ObjectId.isValid(listingId)) {
+        return next(errorHandler(400, 'Invalid listing ID'));
+    }
+    // check if listing exists
+    const listing = await Listing.findById(req.params.id);
+    if(!listing) {
+        console.log("Listing not found!")
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    // check if logged in user owns listing via cookie
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true},
+        )
+        res.status(200).json(updatedListing)
+        
+    } catch (error) {
+        next(error);
     }
 }
