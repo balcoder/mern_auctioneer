a48cdb4fba62e2407589eef14e38f1e2d0c24361
balcoder <des.barrett@gmail.com>
2024-07-19 07:48:39 +0100

    added contact landlord to the listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser, getUserListings} from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser, getUserListings, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id',verifyToken, deleteUser);
 // get all listing of user
 router.get('/listing/:id', verifyToken, getUserListings);
+router.get('/:id', verifyToken, getUser)
 
 
 export default router;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
@@ -13,16 +14,17 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
+  const { currentUser } = useSelector((state) => state.user);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [contact, setContact] = useState(false);
   const [copied, setCopied] = useState(false);
   const params = useParams();
-
-  console.log(listing);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -130,6 +132,17 @@ export default function Listing() {
                 {listing.parking ? "Parking" : "No Parking"}
               </li>
             </ul>
+            {/* check if user logged in and is not the owner and if the contact
+              button has already been clicked and then show contact form */}
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95"
+              >
+                Contact Landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
