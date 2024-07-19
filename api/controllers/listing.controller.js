39f6fb00eba8ae24e2e60a8f49d4b68e718feef8
balcoder <des.barrett@gmail.com>
2024-07-19 09:00:45 +0100

    created api search route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -75,3 +75,51 @@ export const getListing = async (req, res, next) => {
     }
   };
 
+  export const getListings = async (req, res, next) => {    
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        // setup default behaviour of search for all our checkboxes 
+        let offer = req.query.offer;
+        if(offer === undefined || offer === 'false') {
+            // if offer unchecked or searching from home page(undefined)
+            // search for both offer and no offer
+            offer = {$in: [false, true]}
+        }
+
+        let furnished = req.query.furnished;
+        if(furnished === undefined || furnished === 'false') {            
+            furnished = {$in: [false, true]}
+        }
+
+        let parking = req.query.parking;
+        if(parking === undefined || parking === 'false') {            
+            parking = {$in: [false, true]}
+        }
+
+        let type = req.query.type;
+        if(type === undefined || type === 'all') {            
+            type = {$in: ['sale', 'rent']}
+        }
+
+        const searchTerm = req.query.searchTerm || '';        
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+        const listings = await Listing.find({
+            name: {$regex: searchTerm, $options: 'i'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort({
+            [sort]: order
+        }).limit(limit).skip(startIndex);
+        
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+        
+    }
+  };
+
